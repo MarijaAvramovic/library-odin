e45d9c02f8716d79d558bb411942bc070e0c56e6
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2025-11-06 23:12:04 -0500

    add

---
+++ main.js
@@ -0,0 +1,64 @@
+console.log("test");
+
+let library = [];
+const table = document.querySelector('.display-table');
+console.log(table);
+
+function Book(author, title, pages, read) {
+    if(!new.target) {
+        throw Error("You must use the new");
+    }
+    this.id = crypto.randomUUID();
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBooktoLibrary(author, title, pages, read) {
+    const book = new Book(author, title, pages, read);
+    library.push(book);
+}
+
+function getBook(id) {
+   const mybook = library.filter(book => book.title === id);
+    return mybook[0];
+}
+
+function displayB(book) {
+    const row = document.createElement('tr');
+        const dataAuthor = document.createElement('td');
+        const dataTitle = document.createElement('td');
+        const dataPages = document.createElement('td');
+        const dataRead = document.createElement('td');
+
+        dataAuthor.textContent = book.author;
+        dataTitle.textContent = book.title;
+        dataPages.textContent = book.pages;
+        dataRead.textContent = book.read;
+
+        row.appendChild(dataAuthor);
+        row.appendChild(dataTitle);
+        row.appendChild(dataPages);
+        row.appendChild(dataRead);
+
+        table.appendChild(row);
+
+        console.log("ff");
+}
+
+function displayLibrary() {
+    library.forEach(book => {
+        
+    });
+}
+
+
+addBooktoLibrary("Vadim Zeland", "Reality Transurfing", 844, true);
+addBooktoLibrary("Vadim Zeland", "Tufty the Preastest", 144, true);
+addBooktoLibrary("Bentino Massaro", "Me", 1244, false);
+
+// test
+console.log(library);
+console.log(getBook('Tufty the Preastest'));
+displayB(getBook('Tufty the Preastest'));
